bcfdf379534303c2570fd17a17b06c821255b7c1
shaean56 <111380122+shaean56@users.noreply.github.com>
2022-09-04 03:47:00 +0800

    Delete sms.ir.py

--- sms.ir.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-number = input("Enter Your Phone number (without 0) : ")
-url_divar = "https://api.divar.ir/v5/auth/authenticate"
-json_divar = {"phone":number}
-
- req = requests.post(url=url_divar,json=json_divar)
- print(req)
